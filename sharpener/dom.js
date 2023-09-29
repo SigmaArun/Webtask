415f54793135cfd03e43b644fb35023665354dde
SigmaArun <akchaudhary695@gmail.com>
2023-09-29 19:31:23 +0530

    Task6

--- sharpener/dom.js
+++ sharpener/dom.js
@@ -1,3 +1,44 @@
+//QUERY SELECTOR :::::::::::::::::::::::::::::::::::::::
+
+// second item green 
+var item=document.querySelector('.list-group-item:nth-child(2)');
+item.style.backgroundColor='green';
+
+// making third item invisible sharpener 
+var thirditem= document.querySelector('.list-group-item:nth-child(3)')
+ thirditem.style.display='none';
+
+// checking item third on console yes it is there but not visible on form
+ var li =document.getElementsByTagName('li');
+console.log(li);     
+
+// QUERY SELECTORALL ::::::::::::::::::::::::::::::::::::::::::::::::
+// CHANGING SECOND ITEM FORNT GFREEN
+
+//var titles=document.querySelectorAll('.list-group-item');
+ //console.log(titles);
+ var seconditem=document.querySelectorAll('li:nth-child(2)');
+   //seconditem.style.color='green';     // this logic is not working here
+
+   var listItems = document.querySelectorAll('.list-group-item');  // this logic is working
+       if (listItems.length >= 2) {
+ 
+       listItems[1].style.color = 'green';
+       }
+    // ALL ODD GREEN   
+    var odd=document.querySelectorAll('li:nth-child(odd)');
+    for(var i=0 ;i<odd.length;i++){
+        odd[i].style.backgroundColor='green';
+    }
+
+
+
+
+
+
+
+/*
+
 // GET ELEMENT BY TAG NAME ::::::::::::::::::
   var items=  document.getElementsByClassName('list-group-item');
   console.log(items);
@@ -15,21 +56,9 @@
 
  }  */
 
- li[4].style.backgroundColor='green';
-
+ //li[4].style.backgroundColor='green';
   
-
-
-
-
-
-
-
-
-
-
-
-
+  
 /*
 console.log(document.title);
 document.title='Arun';
