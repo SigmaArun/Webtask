818a49263e1d3d70ca589c6b72ba03477645ad44
SigmaArun <akchaudhary695@gmail.com>
2023-10-03 17:00:42 +0530

    task9js

--- sharpener/main.js
+++ sharpener/main.js
@@ -1,3 +1,34 @@
+
+// TASK 9 FILTER SHARPENER ::::::::::::::::::::::::::::::::::::::::::::::::::
+// i am adding filter 
+var filter= document.getElementById('filter');    // defined kar liya filter
+filter.addEventListener('keyup',filterItems);
+// now i am adding function which will invoke by keyup 
+
+function filterItems(e){
+    var text = e.target.value.toLowerCase();
+   // console.log(text);
+    var items =itemList.getElementsByTagName('li');
+    
+  
+    Array.from(items).forEach(function(item){
+    var itemName=item.firstChild.textContent;
+    var itemName2=item.childNodes[1].textContent;  //TASK 9 
+    
+    if(itemName.toLowerCase().indexOf(text) != -1 ||itemName2.toLowerCase().indexOf(text) != -1 ){
+      item.style.display='block';
+    }else{
+      item.style.display='none';
+    }
+    });
+    // adding
+   
+ }
+
+ 
+
+
+
 var form =document.getElementById('addForm');
 var itemList=document.getElementById('items');
 
@@ -15,15 +46,21 @@ function addItem(e){
     var newItem=document.getElementById('item').value;
     // i have to create an li element to add value and show it
     var li=document.createElement('li');
-
-   
-   
+     
     // adda cklass 
     li.className='list-group-item';  // class same like other li tyhen good 
 
     // text node add 
     li.appendChild(document.createTextNode(newItem));
    // itemList.appendChild(li);    // added but i ahve to add a delete button 
+
+// FUNCTIONALITY OF TASK 9 ADDING DESCRIPTION :::::::::::::::::::
+var newItem2=document.getElementById('item2').value;
+//var li=document.createElement('li');
+li.className='list-group-item'; 
+li.appendChild(document.createTextNode(newItem2));
+
+
    
    // logic to creatye delete button
    var deletebtn=document.createElement('button');
@@ -64,7 +101,7 @@ function removeItem(e){
         }
     }
 }
-
+ 
 // ADDING EDIT BUTTON 
 /*
 var li=document.createElement('li');
@@ -74,3 +111,5 @@ editbtn.className='btn btn-danger btn-sm float-right delete';
 editbtn.appendChild(document.createTextNode('Edit'));
 li.appendChild(editbtn);
 itemList.appendChild(li); */
+
+
