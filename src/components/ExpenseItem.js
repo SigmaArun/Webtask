6a51abdd2feaf6442e274e946d917e18e2ca010a
SigmaArun <akchaudhary695@gmail.com>
2024-02-09 05:11:26 +0530

    taskreact

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,12 @@
 function ExpenseItem() {
-  return (
-    <div>
-      <h2>Expense item !</h2>
-      <p>Food Rs 10</p>
-      <p>Petrol Rs 100</p>
-      <p>Movie Rs 200</p>
-    </div>
-  );
-}
-export default ExpenseItem;
+    return (
+      <div>
+        <h2>Expense item !</h2>
+        <p>Food Rs 10</p>
+        <p>Petrol Rs 100</p>
+        <p>Movie Rs 200</p>
+      </div>
+    );
+  }
+  export default ExpenseItem;
+  
