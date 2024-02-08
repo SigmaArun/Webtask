428f95bef3f5fa943ff9ea2f6d3196fe69364c8f
SigmaArun <akchaudhary695@gmail.com>
2024-02-09 04:43:41 +0530

    task7React

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <h2>Expense item !</h2>
+      <p>Food Rs 10</p>
+      <p>Petrol Rs 100</p>
+      <p>Movie Rs 200</p>
+    </div>
+  );
+}
+export default ExpenseItem;
