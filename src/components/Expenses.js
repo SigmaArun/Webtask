d86520aafa857588c8cefdf985182d03cbd62a8d
SigmaArun <akchaudhary695@gmail.com>
2024-02-13 10:33:23 +0530

    task11Cardlogic

---
+++ src/components/Expenses.js
@@ -0,0 +1,38 @@
+import ExpenseItem from "./ExpenseItem";
+import './Expenses.css';
+import Card from "./Card";
+
+function Expenses(props){
+    
+    
+    const keyValue=Object.keys(props.expenses);
+    const expenseItems = [];
+    for(let i=0;i<keyValue.length;i++)       
+    {
+          const key=keyValue[i];
+          if(key){
+            expenseItems.push(
+                <ExpenseItem
+                id={props.expenses[key].id}
+                title={props.expenses[key].title}
+                amount={props.expenses[key].amount}
+                date={props.expenses[key].date}
+                location={props.expenses[key].locationOfExpenditure}
+              ></ExpenseItem>
+            )
+          }
+    }
+    return (
+    
+     
+        <Card className="expenses">
+        
+              {expenseItems}
+        </Card>
+      
+      
+      
+    );
+
+}
+export default Expenses;
