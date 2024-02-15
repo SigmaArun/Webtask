98ecfff9733a805223fd0f390b78e39cef667436
SigmaArun <akchaudhary695@gmail.com>
2024-02-15 22:53:44 +0530

    advancereacttask1

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,18 +5,26 @@ import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
-const ExpenseItem=(props)=> {
+const ExpenseItem=(props)=> {  
+
+  const clickHandler=()=>{
+    
+    console.log('Expense Deleted')
+  }
+
+
   return (
     <Card className="expense-item">
        
-      <ExpenseDate date={props.date} />
+      <ExpenseDate date={props.date} /> 
       
       <ExpenseDetails 
         title={props.title}
         location={props.location}
         amount={props.amount}
       />
-    
+       
+       <button onClick={clickHandler}>Delete Expense</button>
     </Card>
   );
 }
