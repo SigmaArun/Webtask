e5d897dff13bf928be43fcd5ad1f8bd0702aa4bc
SigmaArun <akchaudhary695@gmail.com>
2024-02-17 01:56:11 +0530

    advanceReacttask2amount

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,31 +1,35 @@
+import React, { useState } from "react";
 import ExpenseDetails from "./ExpenseDetails";
 import ExpenseDate from "./ExpenseDate";
-
 
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
-const ExpenseItem=(props)=> {  
+const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+  const [amount ,setAmount]=useState(props.amount);
 
-  const clickHandler=()=>{
+  const clickHandler = () => {
+    setTitle("updated!");
     
-    console.log('Expense Deleted')
+    console.log(title);
+  };
+
+  const amountHandler=()=>{
+    setAmount('100');
   }
-
-
   return (
     <Card className="expense-item">
-       
-      <ExpenseDate date={props.date} /> 
-      
-      <ExpenseDetails 
-        title={props.title}
+      <ExpenseDate date={props.date} />
+
+      <ExpenseDetails
+        title={title}
         location={props.location}
-        amount={props.amount}
+        amount={amount}
       />
-       
-       <button onClick={clickHandler}>Delete Expense</button>
+      <button onClick={amountHandler}>ADD Amount</button>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
-}
+};
 export default ExpenseItem;
