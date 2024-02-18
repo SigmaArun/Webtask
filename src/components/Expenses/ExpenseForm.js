51a2cac8ae2efee3999d778cd482912cfe741690
SigmaArun <akchaudhary695@gmail.com>
2024-02-19 02:12:53 +0530

    usestatewithobject

--- src/components/Expenses/ExpenseForm.js
+++ src/components/Expenses/ExpenseForm.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../UI/Card';
 const ExpenseForm = () => {
-  const submitForm = (event) => {
+
+  const [enteredTitle,setEnteredTitle]=useState('');
+  const [enteredAmount,setEnteredAmount]=useState('');
+  const [enteredDate,setEnteredDate]=useState('');
+
+   const titleChangeHandler=(event)=>{
+    //console.log('Title changed:', event.target.value);
+     setEnteredTitle(event.target.value);
+   };
+   const amountChangeHandler=(event)=>{
+     setEnteredAmount(event.target.value)
+   };
+   const dateChangeHandler=(event)=>{
+   setEnteredDate(event.target.value)
+   };
+   
+   const submitForm = (event) => {
     event.preventDefault();
-    const title = event.target.title.value;
-    const date = event.target.date.value;
-    const amount = event.target.amount.value;
+    const title = enteredTitle;
+    const date = enteredDate;
+
+    const amount = enteredAmount;
     const obj = {
     
       date,
@@ -13,18 +30,18 @@ const ExpenseForm = () => {
       amount
     };
     localStorage.setItem(obj.date, JSON.stringify(obj));
-    console.log(obj)
-  };
+    console.log(obj)   
+  };  
   return (
     <Card className="expenses">
     <div>
-      <form onSubmit={submitForm}>
+      <form  onSubmit={submitForm}>
         <label htmlFor="date">Date</label>
-        <input type="date" name=" date" id="date"></input>
+        <input type="date" name=" date" id="date" onChange={dateChangeHandler}></input>
         <label htmlFor="title">title</label>
-        <input type="text" name="title" id="title"></input>
+        <input type="text" name="title" id="title" onChange={titleChangeHandler}></input>
         <label htmlFor="amount">Amount</label>
-        <input type="number" name=" amount" id="amount"></input>
+        <input type="number" name=" amount" id="amount" onChange={amountChangeHandler}></input>
         <button type="submit">Add Expense</button>
       </form>
     </div>
