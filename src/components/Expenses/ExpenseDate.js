631b6a55c0274b51557e96c80f0967823e3d3a7a
SigmaArun <akchaudhary695@gmail.com>
2024-02-19 23:09:47 +0530

    React app second part

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -7,7 +7,7 @@ const ExpenseDate=(props)=>{
     return(
         
         <div className="expense-date">
-          <div className="expense-date__month">{month}</div>
+           <div className="expense-date__month">{month}</div>
           <div className="expense-date__year">{year}</div>
           <div className="expense-date__day">{day}</div>
         </div>
--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -3,10 +3,11 @@ import './ExpenseItem.css';
    
     return(
         <div className='expense-item__description'>
-        <div className='expense-item__description'> 
-        <h2>{props.title} </h2></div>
-        <div className='expense-item__description'> <h2>{props.location} </h2></div>
-        <div className='expense-item__price'>${props.amount}</div>
+            <div className='expense-item__description'> 
+                <h2>{props.title} </h2>
+            </div>
+           <div className='expense-item__description'> <h2>{props.location} </h2></div>
+            <div className='expense-item__price'>${props.amount}</div>
          </div>
     )
  }
