40aefe0c4cb2d96919f067262c76ca4349ee2e05
SigmaArun <akchaudhary695@gmail.com>
2024-02-21 05:11:23 +0530

    React app fifth push using map

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,53 +1,32 @@
 import ExpenseItem from "./ExpenseItem";
-import './Expenses.css';
+import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-const Expenses=(props)=>{
-   
-  const [filteredYear,setFilteredYear]= useState('2020');
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
 
-   const filteredChangeHandler= selectedYear=>{
+  const filteredChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
-   };
-    
-    
-    const keyValue=Object.keys(props.expenses);
-    const expenseItems = [];
-    for(let i=0;i<keyValue.length;i++)       
-    {
-          const key=keyValue[i];
-          const expenseYear = props.expenses[key].date.getFullYear().toString();
-          if(key && expenseYear === filteredYear){
-            expenseItems.push(
-            
-                <ExpenseItem
-                key={key}  
-                id={props.expenses[key].id}
-                title={props.expenses[key].title}
-                amount={props.expenses[key].amount}
-                date={props.expenses[key].date}
-                location={props.expenses[key].locationOfExpenditure}
-              ></ExpenseItem>
-             
-            )
-          }
-          
-    }
-    return (
-    
-       
-        <Card className="expenses">
-            <ExpensesFilter selected={filteredYear} onChangeFilter={filteredChangeHandler}/>
-        
-              {expenseItems}
-          
-        </Card>
-      
-      
-      
-    );
+  };
 
-}
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filteredChangeHandler}
+      />
+
+      {props.items.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </Card>
+  );
+};
 export default Expenses;
