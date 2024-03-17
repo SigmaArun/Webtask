c9c39ff9a8be5c15047b626fac5eca841d213c1c
SigmaArun <akchaudhary695@gmail.com>
2024-03-18 00:02:20 +0530

    summary added in resturant app

--- src/002 01-starting-project/01-starting-project/src/components/Layout/Header.js
+++ src/002 01-starting-project/01-starting-project/src/components/Layout/Header.js
@@ -2,6 +2,7 @@ import React, { Fragment } from "react";
 import mealsImage from '../../assests/meals.jpg';
 import classes from './Header.module.css';
 import HeaderCartButton from "./HeaderCartButton";
+import Card from "../UI/Card";
 
  const Header=(props)=>{
     return(
@@ -12,6 +13,21 @@ import HeaderCartButton from "./HeaderCartButton";
         </header>
         <div className={classes['main-image']}>
             <img src={mealsImage} alt="a table full of delicious food"></img>
+        </div>
+        <div className={classes.centered}>
+        <Card>
+          <h1>Delicious Food ,Delivered to you</h1>
+          
+          <p>Choose your favourite meals from our broad selection of avilable meals 
+             and enjoy a delicious</p> 
+           <p>  lunch or dinner at Home</p>
+          
+          <p>All our meals are cooked with high quality ingredients ,just in time 
+            and ofCourse by </p>
+          <p>  experienced chefs!
+          </p>
+        
+        </Card>
         </div>
        
       </Fragment>
